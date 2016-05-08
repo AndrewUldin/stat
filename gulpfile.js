b874ded68f34423a218d4787107c68f256c9544a
Andrew Uldin <uldin@yandex-team.ru>
2016-05-08 18:58:53 +0300

    Temp

--- gulpfile.js
+++ gulpfile.js
@@ -11,13 +11,31 @@ var assign = require('lodash.assign');
 var sass = require('gulp-sass');
 var reactify = require('reactify');
 var babelify = require('babelify');
+var uglify = require('gulp-uglify');
+var postcss = require('gulp-postcss');
+var autoprefixer = require('autoprefixer');
+var cssnano = require('cssnano');
+
+var env = (process.env.NODE_ENV === 'production' ? 'production' : 'development');
 
 // add custom browserify options here
-var customOpts = {
-    entries: ['./src/js/index.js'],
-    debug: true,
-    cache: {}, packageCache: {}, fullPaths: true // Requirement of watchify
-};
+if (env == 'development') {
+    var customOpts = {
+        entries: ['./src/js/index.js'],
+        debug: true,
+        cache: {},
+        packageCache: {},
+        fullPaths: true // Requirement of watchify
+    };
+} else {
+    var customOpts = {
+        entries: ['./src/js/index.js'],
+        debug: false,
+        cache: {},
+        packageCache: {},
+        fullPaths: false
+    };
+}
 var opts = assign({}, watchify.args, customOpts);
 var b = watchify(browserify(opts))
 .transform('babelify', {presets: ['es2015', 'react']})
@@ -26,22 +44,21 @@ var b = watchify(browserify(opts))
 gulp.task('js', bundle); // so you can run `gulp js` to build the file
 b.on('update', bundle); // on any dep update, runs the bundler
 b.on('log', gutil.log); // output build logs to terminal
-// b.on('error', gutil.log.bind(gutil, 'Browserify Error'));
 
 function bundle() {
-    return b
-        .bundle()
+    return b.bundle()
         .on('error', gutil.log.bind(gutil, 'Browserify Error'))
         .pipe(source('index.js'))
-        // optional, remove if you don't need to buffer file contents
-        .pipe(buffer())
-        // optional, remove if you dont want sourcemaps
-        .pipe(sourcemaps.init({
-            loadMaps: true
-        })) // loads map from browserify file
-        // Add transformation tasks to the pipeline here.
-        .pipe(sourcemaps.write('./')) // writes .map file
-        .pipe(gulp.dest('./app/scripts'));
+        if (env == 'development') {
+            b.pipe(sourcemaps.init({
+                loadMaps: true
+            }))
+            .pipe(sourcemaps.write('./')) // writes .map file
+        } else {
+            b.pipe(buffer());
+            b.pipe(uglify());
+        }
+        b.pipe(gulp.dest('./app/scripts'));
 }
 
 gulp.task('sass', function() {
@@ -49,6 +66,16 @@ gulp.task('sass', function() {
         .pipe(sourcemaps.init())
         .pipe(sass().on('error', sass.logError))
         .pipe(sourcemaps.write())
+        .pipe(gulp.dest('./app/styles'));
+});
+
+gulp.task('sass:min', function () {
+    var processors = [
+        autoprefixer({browsers: ['last 1 version']}),
+        cssnano(),
+    ];
+    return gulp.src('./app/styles/*.css')
+        .pipe(postcss(processors))
         .pipe(gulp.dest('./app/styles'));
 });
 
